eb0c2823c917e8f4de3fbdbae2e5c65c838828e2
Alisha Mullick <111171569+alishamullick2002@users.noreply.github.com>
2026-06-26 15:35:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "alisha-terraform-state-bucket"
     key    = "terraform.tfstate"
-    region = "us-west-2"
+    region = "us-east-1"
     encrypt = true
   }
 }
